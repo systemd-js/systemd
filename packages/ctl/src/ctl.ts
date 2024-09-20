066597677d5bfc4c4a7fd6d00ba6a673a2fe1fec
chyzwar <schizek.marcin@gmail.com>
2024-09-20 23:52:15 +0200

    feat(ctl): added isActive ctl function

--- packages/ctl/src/ctl.ts
+++ packages/ctl/src/ctl.ts
@@ -56,68 +56,6 @@ function getUnit(unitName: string, type: string = getType(unitName)): Unit | und
   }
 };
 
-export class Ctl {
-  private readonly type: string;
-  private readonly name: string;
-  private readonly path: string;
-
-  private readonly unit?: Unit;
-  private readonly current?: Unit;
-
-  public constructor(name: string, unit?: Unit) {
-    this.name = getName(name);
-    this.type = getType(name, unit);
-    this.current = getUnit(
-      this.name,
-      this.type,
-    );
-    this.path = getPath(
-      this.name,
-      this.type,
-    );
-    this.unit = unit;
-  }
-
-  /**
-   * Create the unit file if it does not exist or if it is different from the current unit.
-   */
-  public create() {
-    if (!this.unit) {
-      throw new Error("Unit not found");
-    }
-    const unitString = this.unit.toINIString();
-    const currentUnit = this.current?.toINIString();
-
-    if (currentUnit !== unitString) {
-      writeFileSync(this.path, unitString);
-    }
-  }
-
-  public enable() {
-    execSync(`systemctl enable ${this.name}.${this.type}`);
-  }
-
-  public disable() {
-    execSync(`systemctl disable ${this.name}.${this.type}`);
-  }
-
-  public start() {
-    execSync(`systemctl start ${this.name}.${this.type}`);
-  }
-
-  public stop() {
-    execSync(`systemctl stop ${this.name}.${this.type}`);
-  }
-
-  public restart() {
-    execSync(`systemctl restart ${this.name}.${this.type}`);
-  }
-
-  public reload() {
-    execSync(`systemctl daemon-reload ${this.name}.${this.type}`);
-  }
-}
-
 /**
  * Create a unit file if it does not exist or if it is different from the current unit.
  */
@@ -179,4 +117,77 @@ export function restart(unitName: string, unit?: Unit) {
   const name = getName(unitName);
 
   execSync(`systemctl restart ${name}.${type}`);
+}
+
+export function isActive(unitName: string, unit?: Unit) {
+  const type = getType(unitName, unit);
+  const name = getName(unitName);
+
+  return execSync(`systemctl is-active ${name}.${type}`).toString().trim() === "active";
+}
+
+export class Ctl {
+  private readonly type: string;
+  private readonly name: string;
+  private readonly path: string;
+
+  private readonly unit?: Unit;
+  private readonly current?: Unit;
+
+  public constructor(name: string, unit?: Unit) {
+    this.name = getName(name);
+    this.type = getType(name, unit);
+    this.current = getUnit(
+      this.name,
+      this.type,
+    );
+    this.path = getPath(
+      this.name,
+      this.type,
+    );
+    this.unit = unit;
+  }
+
+  /**
+   * Create the unit file if it does not exist or if it is different from the current unit.
+   */
+  public create() {
+    if (!this.unit) {
+      throw new Error("Unit not found");
+    }
+    const unitString = this.unit.toINIString();
+    const currentUnit = this.current?.toINIString();
+
+    if (currentUnit !== unitString) {
+      writeFileSync(this.path, unitString);
+    }
+  }
+
+  public enable() {
+    execSync(`systemctl enable ${this.name}.${this.type}`);
+  }
+
+  public disable() {
+    execSync(`systemctl disable ${this.name}.${this.type}`);
+  }
+
+  public start() {
+    execSync(`systemctl start ${this.name}.${this.type}`);
+  }
+
+  public stop() {
+    execSync(`systemctl stop ${this.name}.${this.type}`);
+  }
+
+  public restart() {
+    execSync(`systemctl restart ${this.name}.${this.type}`);
+  }
+
+  public reload() {
+    execSync(`systemctl daemon-reload ${this.name}.${this.type}`);
+  }
+
+  public isActive() {
+    return isActive(this.name, this.unit);
+  }
 }
